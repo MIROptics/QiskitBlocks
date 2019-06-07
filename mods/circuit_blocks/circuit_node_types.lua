1fb4b56fc33f25ebafa0399cd5c10b00e8844187
JavaFXpert <james@j1w.xyz>
2019-06-07 12:44:44 -0400

    Begin populating node metadata

---
+++ mods/circuit_blocks/circuit_node_types.lua
@@ -0,0 +1,17 @@
+-- CircuitNodeTypes definitions
+CircuitNodeTypes = {
+    EMPTY = -1,
+    IDEN = 0,
+    X = 1,
+    Y = 2,
+    Z = 3,
+    S = 4,
+    SDG = 5,
+    T = 6,
+    TDG = 7,
+    H = 8,
+    SWAP = 9,
+    -- B = 10,
+    CTRL = 11, -- "control" part of multi-qubit gate
+    TRACE = 12 -- In the path between a gate part and a "control" or "swap" part
+}
--- mods/circuit_blocks/init.lua
+++ mods/circuit_blocks/init.lua
@@ -1,3 +1,5 @@
+dofile(minetest.get_modpath("circuit_blocks").."/circuit_node_types.lua");
+
 minetest.register_node("circuit_blocks:qubit_0", {
     description = "Qubit 0 block",
     tiles = {"qubit_0.png"},
@@ -13,7 +15,27 @@ minetest.register_node("circuit_blocks:qubit_1", {
 minetest.register_node("circuit_blocks:x_gate", {
     description = "Pauli X gate block",
     tiles = {"x_gate.png"},
-    groups = {oddly_breakable_by_hand=2}
+    groups = {oddly_breakable_by_hand=2},
+    on_construct = function(pos)
+		local meta = minetest.get_meta(pos)
+        meta:set_int("node_type", CircuitNodeTypes.X)
+		meta:set_float("radians", 0.0)
+		meta:set_int("ctrl_a", -1)
+		meta:set_int("ctrl_b", -1)
+        minetest.debug("Set meta for X gate")
+	end,
+    on_punch = function(pos, node, player)
+		local meta = minetest.get_meta(pos)
+		local node_type = meta:get_int("node_type")
+		local radians = meta:get_float("radians")
+		local ctrl_a = meta:get_int("ctrl_a")
+		local ctrl_b = meta:get_int("ctrl_b")
+        minetest.debug("node_type: " .. tostring(ctrl_a) ..
+                ", radians: " .. tostring(radians) ..
+                ", ctrl_a: " .. tostring(ctrl_a) ..
+                ", ctrl_b: " .. tostring(ctrl_b))
+        return
+    end
 })
 
 minetest.register_node("circuit_blocks:h_gate", {
