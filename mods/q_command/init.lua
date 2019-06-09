fcbfd6692defc3d42f8cc197d8dcc34cf3a69bc2
JavaFXpert <james@j1w.xyz>
2019-06-09 18:17:59 -0400

    Begin creating Q command block formspec

--- mods/q_command/init.lua
+++ mods/q_command/init.lua
@@ -9,7 +9,15 @@ minetest.debug("In q_command/init.lua")
 minetest.register_node("q_command:q_block", {
     description = "Q command block",
     tiles = {"q_command_block.png"},
-    groups = {oddly_breakable_by_hand=2}
+    groups = {oddly_breakable_by_hand=2},
+    on_construct = function(pos)
+        local meta = minetest.get_meta(pos)
+        meta:set_string("infotext", "Quantum circuit command block");
+        meta:set_string("formspec",
+                "size[8,9]"..
+                "field[0.3,3.5;2.2,1;;label:;'foo']"..
+                "field[0.3,4.5;2.2,1;;label2:;'bar']")
+    end
 })
 
 
