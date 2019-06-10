567c8ba0ee5111228edc998db96b0738ce569d50
JavaFXpert <james@j1w.xyz>
2019-06-10 06:39:59 -0400

    User may enter offsets

--- mods/q_command/init.lua
+++ mods/q_command/init.lua
@@ -8,10 +8,14 @@ minetest.debug("In q_command/init.lua")
 
 minetest.register_on_player_receive_fields(function(player, formname, fields)
     if(formname == "create_circuit_grid") then
-        if fields.num_wires and fields.num_columns then
+        if fields.num_wires and fields.num_columns and
+                fields.start_z_offset and fields.start_x_offset then
             minetest.chat_send_player(player:get_player_name(),
-                    "Creating circuit grid with " .. fields["num_wires"] ..
-                            " wires and " .. fields["num_columns"] .. " columns"
+                    "Creating circuit grid with " ..
+                            fields["num_wires"] .. " wires, " ..
+                            fields["num_columns"] .. " columns, " ..
+                            fields["start_z_offset"] .. " start_z_offset, " ..
+                            fields["start_x_offset"] .. " start_x_offset"
             )
             return
         end
@@ -30,8 +34,10 @@ minetest.register_node("q_command:q_block", {
         local player_name = clicker:get_player_name()
         local meta = minetest.get_meta(pos)
         local formspec = "size[5.0, 4.6]"..
-                "field[1.0,0.5;1.5,1.0;num_wires;Wires:;3]" ..
-                "field[3.0,0.5;1.5,1.0;num_columns;Columns:;8]" ..
+                "field[1.0,0.5;1.5,1.5;num_wires;Wires:;3]" ..
+                "field[3.0,0.5;1.5,1.5;num_columns;Columns:;8]" ..
+                "field[1.0,2.0;1.5,1.5;start_z_offset;Forward offset:;3]" ..
+                "field[3.0,2.0;1.5,1.5;start_x_offset;Left offset:;4]" ..
 				"button_exit[1.8,3.5;1.5,1.0;create;Create]"
         minetest.show_formspec(player_name, "create_circuit_grid", formspec)
     end
