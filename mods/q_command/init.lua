5ccd617f78e3c6f558b4b657a6962b9d4d4aa32a
JavaFXpert <james@j1w.xyz>
2019-06-09 20:56:45 -0400

    Continue creating Q command block formspec

--- mods/q_command/init.lua
+++ mods/q_command/init.lua
@@ -6,6 +6,27 @@ q_command = {}
 
 minetest.debug("In q_command/init.lua")
 
+minetest.register_on_player_receive_fields(function(player, formname, fields)
+	if(fields.leave) then
+		minetest.kick_player(player:get_player_name(), S("You have voluntarily exited the tutorial."))
+		return
+	elseif(fields.teleport) then
+		teleport_dialog(player)
+		return
+	elseif(fields.gotoend) then
+		tutorial.go_to_end(player)
+		return
+	end
+	if formname == "tutorial_teleport" then
+		for id, data in pairs(tutorial.locations) do
+			if(fields["teleport_"..id]) then
+				tutorial.teleport(player, data[1], data[2], data[3])
+				break
+			end
+		end
+	end
+end)
+
 minetest.register_node("q_command:q_block", {
     description = "Q command block",
     tiles = {"q_command_block.png"},
@@ -14,9 +35,11 @@ minetest.register_node("q_command:q_block", {
         local meta = minetest.get_meta(pos)
         meta:set_string("infotext", "Quantum circuit command block");
         meta:set_string("formspec",
-                "size[8,9]"..
-                "field[0.3,3.5;2.2,1;;label:;'foo']"..
-                "field[0.3,4.5;2.2,1;;label2:;'bar']")
+                "size[3.0, 5.0]"..
+                "field[1.0, 0.5; 1.5, 1.0; num_wires; Wires:; 3]" ..
+                "field[1.0, 2.0; 1.5, 1.0; num_columns; Columns:; 8]" ..
+				"button_exit[1.0, 3.5; 1.5, 1.0; create_circuit_grid; Create]"
+		)
     end
 })
 
