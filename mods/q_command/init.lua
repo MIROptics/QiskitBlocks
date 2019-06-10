4a665c4a504c29943ad767fc449c477a776f66ad
JavaFXpert <james@j1w.xyz>
2019-06-09 22:34:28 -0400

    Continue creating Q command block formspec

--- mods/q_command/init.lua
+++ mods/q_command/init.lua
@@ -7,24 +7,10 @@ q_command = {}
 minetest.debug("In q_command/init.lua")
 
 minetest.register_on_player_receive_fields(function(player, formname, fields)
-	if(fields.leave) then
-		minetest.kick_player(player:get_player_name(), S("You have voluntarily exited the tutorial."))
-		return
-	elseif(fields.teleport) then
-		teleport_dialog(player)
-		return
-	elseif(fields.gotoend) then
-		tutorial.go_to_end(player)
-		return
-	end
-	if formname == "tutorial_teleport" then
-		for id, data in pairs(tutorial.locations) do
-			if(fields["teleport_"..id]) then
-				tutorial.teleport(player, data[1], data[2], data[3])
-				break
-			end
-		end
-	end
+    if(formname == "create_circuit_grid") then
+        minetest.chat_send_player(player:get_player_name(), "Creating circuit grid .. ")
+        return
+    end
 end)
 
 minetest.register_node("q_command:q_block", {
@@ -34,20 +20,14 @@ minetest.register_node("q_command:q_block", {
     on_construct = function(pos)
         local meta = minetest.get_meta(pos)
         meta:set_string("infotext", "Quantum circuit command block")
-        --meta:set_string("formspec",
-        --        "size[3.0, 5.0]"..
-        --        "field[1.0, 0.5; 1.5, 1.0; num_wires; Wires:; 3]" ..
-        --        "field[1.0, 2.0; 1.5, 1.0; num_columns; Columns:; 8]" ..
-		--		"button_exit[1.0, 3.5; 1.5, 1.0; create_circuit_grid; Create]"
-		--)
     end,
     on_rightclick = function(pos, node, clicker, itemstack)
         local player_name = clicker:get_player_name()
         local meta = minetest.get_meta(pos)
-        local formspec = "size[3.0, 5.0]"..
+        local formspec = "size[3.0, 4.6]"..
                 "field[1.0, 0.5; 1.5, 1.0; num_wires; Wires:; 3]" ..
                 "field[1.0, 2.0; 1.5, 1.0; num_columns; Columns:; 8]" ..
-				"button_exit[1.0, 3.5; 1.5, 1.0; create; Create]"
+				"button_exit[0.8, 3.5; 1.5, 1.0; create; Create]"
         minetest.show_formspec(player_name, "create_circuit_grid", formspec)
     end
 })
