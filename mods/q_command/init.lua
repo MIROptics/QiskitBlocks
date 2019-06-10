9f59f45ba9e74848b449bb8e7620f266874529b1
JavaFXpert <james@j1w.xyz>
2019-06-09 23:34:17 -0400

    Return fields entered in Q command block formspec

--- mods/q_command/init.lua
+++ mods/q_command/init.lua
@@ -8,8 +8,13 @@ minetest.debug("In q_command/init.lua")
 
 minetest.register_on_player_receive_fields(function(player, formname, fields)
     if(formname == "create_circuit_grid") then
-        minetest.chat_send_player(player:get_player_name(), "Creating circuit grid .. ")
-        return
+        if fields.num_wires and fields.num_columns then
+            minetest.chat_send_player(player:get_player_name(),
+                    "Creating circuit grid with " .. fields["num_wires"] ..
+                            " wires and " .. fields["num_columns"] .. " columns"
+            )
+            return
+        end
     end
 end)
 
@@ -24,10 +29,10 @@ minetest.register_node("q_command:q_block", {
     on_rightclick = function(pos, node, clicker, itemstack)
         local player_name = clicker:get_player_name()
         local meta = minetest.get_meta(pos)
-        local formspec = "size[3.0, 4.6]"..
-                "field[1.0, 0.5; 1.5, 1.0; num_wires; Wires:; 3]" ..
-                "field[1.0, 2.0; 1.5, 1.0; num_columns; Columns:; 8]" ..
-				"button_exit[0.8, 3.5; 1.5, 1.0; create; Create]"
+        local formspec = "size[5.0, 4.6]"..
+                "field[1.0,0.5;1.5,1.0;num_wires;Wires:;3]" ..
+                "field[3.0,0.5;1.5,1.0;num_columns;Columns:;8]" ..
+				"button_exit[1.8,3.5;1.5,1.0;create;Create]"
         minetest.show_formspec(player_name, "create_circuit_grid", formspec)
     end
 })
