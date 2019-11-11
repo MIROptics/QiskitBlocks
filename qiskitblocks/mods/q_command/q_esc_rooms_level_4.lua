5abe6e04d69c28331ce6c37f239c8196e18828ee
JavaFXpert <james@j1w.xyz>
2019-11-11 08:32:51 -0500

    Document code with instructions on modifying escape rooms

--- qiskitblocks/mods/q_command/q_esc_rooms_level_4.lua
+++ qiskitblocks/mods/q_command/q_esc_rooms_level_4.lua
@@ -15,22 +15,46 @@ limitations under the License.
 --]]
 
 --[[
-Elements of the q_command table that supply information about areas in the game
+Elements of the q_command table that supply information about areas in the game.
+The "Note to devs" comments in Room 1 apply to all escape rooms in all levels
 --]]
 
 -- Escape room puzzles Level IV ------------------------------------------------
 -------- Room 1 (Level IV)
+-- Note to devs: Use lev_X_rm_Y pattern, where X is level number and Y is room number
 q_command.areas.lev_4_rm_1 = {}
 q_command.areas.lev_4_rm_1.region = q_command.regions.esc_rooms_level_4
 table.insert(q_command.regions.esc_rooms_level_4, q_command.areas.lev_4_rm_1)
 q_command.areas.lev_4_rm_1.area_num = #q_command.regions.esc_rooms_level_4
 
+-- Note to devs: The center_pos x/y/z values should indicate the position of the center
+-- block in the floor of the room. This is used, along with radius, to identify when a
+-- player is in the room. This enables mechanisms such as greeting the player and
+-- erasing their inventory when they enter an escape room.
 q_command.areas.lev_4_rm_1.center_pos = {x = 238, y = 0, z = 32}
 q_command.areas.lev_4_rm_1.radius = 5
+
+-- Note to devs: The q_block_pos x/y/z values should indicate where the Q block is,
+-- for input to mechanisms such as opening the exit door when a puzzle is solved.
+-- Since the escape rooms in this level are already preloaded with default circuit
+-- puzzles, it will often be necessary to change the q_block_pos when replacing it
+-- with a different sized puzzle. To delete a puzzle from an escape room, hold the
+-- shift key while left-clicking the Q block. To create a circuit puzzle in a room,
+-- search the inventory for a Q block by using the pattern "lev_X_rm_Y", where X is
+-- level number and Y is room number. Right-click to place the Q block in the
+-- desired location in the room, and supply the desired number of rows/columns in the
+-- circuit. You'll need to be in Creative Mode to search the inventory for Q blocks.
 q_command.areas.lev_4_rm_1.q_block_pos = {x = 241, y = 0, z = 34}
 
+-- Note to devs: The door_pos x/y/z values should indicate where the exit door is.
 q_command.areas.lev_4_rm_1.door_pos = {x = 236, y = 0, z = 37}
+
+-- Note to devs: The chest_pos x/y/z values should indicate where the chest is, so
+-- that it may be restocked when a puzzle is solved.
 q_command.areas.lev_4_rm_1.chest_pos = {x = 234, y = 0, z = 34}
+
+-- Note to devs: Remove chest inventory items not required, by making their relevant
+-- strings empty.
 q_command.areas.lev_4_rm_1.chest_inv = {
     inventory = {
         main = {[1] = "", [2] = "", [3] = "", [4] = "", [5] = "", [6] = "",
@@ -58,15 +82,40 @@ q_command.areas.lev_4_rm_1.chest_inv = {
         }
     }
 }
+
+-- Note to devs: The variable ending in solution_unitary holds the unitary matrix
+-- of complex numbers that represent the solution to a circuit puzzle. Use this
+-- variable when the puzzle is expressed in terms of transforming various inputs
+-- to corresponding outputs. For example, the circuit puzzle challenge in this
+-- room is to create a CNOT gate, which is defined by a unique unitary matrix.
+-- To obtain the value to supply here, *temporarily* uncomment the relevant line
+-- in the init.lua file in the q_command directory:
+--      minetest.debug("unitary:\n" .. dump(unitary))
+-- This will cause the unitary matrix to appear in the debug.txt log file every
+-- time the circuit is modified or measured. This matrix can be quite large, so
+-- remove spaces, tabs, and newline characters before inserting here.
 q_command.areas.lev_4_rm_1.solution_unitary =
 {{{r=1,i=0},{r=0,i=0},{r=0,i=0},{r=0,i=0}},{{r=0,i=0},{r=0,i=0},{r=0,i=0},{r=1,i=0}},
 {{r=0,i=0},{r=0,i=0},{r=1,i=0},{r=0,i=0}},{{r=0,i=0},{r=1,i=0},{r=0,i=0},{r=0,i=0}}}
 
--- Note: Use the following as an example if validating player's
--- solution with a statevector simulator rather than a unitary simulator
--- q_command.areas.lev_4_rm_1.solution_statevector =
--- {{r=0,i=0},{r=0,i=0},{r=1,i=0},{r=0,i=0}}
+-- Note to devs: The variable ending in solution_statevector holds the statevector
+-- of complex numbers that represent the solution to a circuit puzzle. Use this
+-- variable when the puzzle is expressed in terms of its desired statevector.
+-- To obtain the value to supply here, *temporarily* uncomment the relevant line
+-- in the init.lua file in the q_command directory:
+--      minetest.debug("statevector:\n" .. dump(statevector))
+-- This will cause the statevector to appear in the debug.txt log file every
+-- time the circuit is modified or measured. This vector can be quite large, so
+-- remove spaces, tabs, and newline characters before inserting here.
+--[[
+q_command.areas.lev_4_rm_1.solution_statevector =
+{{r=0,i=0},{r=0,i=0},{r=1,i=0},{r=0,i=0}}
+--]]
 
+-- Note to devs: The variable ending in help_chat_msg holds the English version of
+-- the message that Professor Q will chat to the player when entering an escape room
+-- for the first time since the application is invoked. Note that for chat messages,
+-- ".en" is *not* added to the end of the variable.
 q_command.areas.lev_4_rm_1.help_chat_msg = {
 	"We meet again, esteemed colleague! You may recall that my name is Professor Q and",
 	"that I'm standing behind this glass because we're conducting quantum experiments.",
@@ -74,6 +123,9 @@ q_command.areas.lev_4_rm_1.help_chat_msg = {
 	"realizes a digital 'exclusive or' gate as shown on the wall. You may place |1>",
 	"blocks on wires to test input combinations without affecting the exit door."
 }
+-- Note to devs: The variable ending in help_chat_msg and a language code holds the
+-- localized version of the message that Professor Q will chat to the player when
+-- entering an escape room for the first time since the application is invoked.
 q_command.areas.lev_4_rm_1.help_chat_msg.ja = {
 	"また会いましたね、尊敬する同僚！私の名前はQ教授で、量子実験を行っているため、このガ",
 	"ラスの後ろに立っていることを思い出してください。私はあなたが元気だと確信しています:-)",
@@ -82,12 +134,20 @@ q_command.areas.lev_4_rm_1.help_chat_msg.ja = {
 	"に|1>のブロックを配置できます。"
 }
 q_command.areas.lev_4_rm_1.help_chat_sent = false
+
+-- Note to devs: The variable ending in help_success_msg holds the English version of
+-- the message that Professor Q will chat to the player when solving an escape room
+-- puzzle for the first time since the application is invoked. Note that for chat
+-- messages, ".en" is *not* added to the end of the variable.
 q_command.areas.lev_4_rm_1.help_success_msg = {
 	"Well done! You may know that the CNOT gate is its own inverse, so supplying the ",
 	"output as input will give you the original input as output. That's reversible ",
 	"computing, with no qubits falling in the bit bucket! Perhaps experiment with ",
 	"that before leaving this room to see reversible computing in action."
 }
+-- Note to devs: The variable ending in help_success_msg and a language code holds the
+-- localized version of the message that Professor Q will chat to the player when
+-- solving an escape room puzzle for the first time since the application is invoked.
 q_command.areas.lev_4_rm_1.help_success_msg.ja = {
 	"よくできました！ CNOTゲートはそれ自体が可逆であることがわかっているため、出力を入力 ",
 	"として提供すると、元の入力が出力として提供されます。 これは、ビットのバケツに量子ビッ ",
@@ -96,6 +156,18 @@ q_command.areas.lev_4_rm_1.help_success_msg.ja = {
 }
 q_command.areas.lev_4_rm_1.success_chat_sent = false
 
+-- Note to devs: The variable ending in help_btn_text followed by a language code
+-- holds the text that appears when right-clicking a help sign that contains a
+-- question mark "?". When used in an escape room, the sign should be located directly
+-- above the chest, and should start with the same message as the chat message held in
+-- the localized help_chat_msg variables discussed previously. If it seems prudent,
+-- additional notes and hints should be included in the text, but be careful not to
+-- make it too easy for the player to solve the puzzle. Note that for help_btn_text
+-- it is necessary to include the language code (".en") when supplying English text.
+-- Whenever you change the text on a sign, delete it from the wall by left-clicking,
+-- and put it back on the wall by wielding the sign and right-clicking. To find the
+-- sign in inventory for a given escape room, search with the pattern "lev_X_rm_Y",
+-- where X is level number and Y is room number.
 q_command.areas.lev_4_rm_1.help_btn_text = {}
 q_command.areas.lev_4_rm_1.help_btn_text.en =
 [[
@@ -152,6 +224,12 @@ Xゲートを制御Xゲートに（およびその逆に）変換するには、
 す。コントロール側が|1>の状態の時、ターゲット側でX操作を実行します。制御量子ビットが
 重ね合わせにある場合、このゲートはエンタングルメント状態を作ります。
 ]]
+
+
+-- Note to devs: The variable ending in help_btn_caption followed by a language code
+-- holds the text that appears in the caption of a help sign. Note that for
+-- help_btn_caption it is necessary to include the language code (".en") when
+-- supplying English text.
 q_command.areas.lev_4_rm_1.help_btn_caption = {}
 q_command.areas.lev_4_rm_1.help_btn_caption.en = "Make a quantum logic XOR gate"
 q_command.areas.lev_4_rm_1.help_btn_caption.es = q_command.areas.lev_4_rm_1.help_btn_caption.en
@@ -200,11 +278,6 @@ q_command.areas.lev_4_rm_2.chest_inv = {
 q_command.areas.lev_4_rm_2.solution_unitary =
 {{{r=0,i=0},{r=0,i=0},{r=1,i=0},{r=0,i=0}},{{r=0,i=0},{r=1,i=0},{r=0,i=0},{r=0,i=0}},
 {{r=1,i=0},{r=0,i=0},{r=0,i=0},{r=0,i=0}},{{r=0,i=0},{r=0,i=0},{r=0,i=0},{r=1,i=0}}}
-
--- Note: Use the following as an example if validating player's
--- solution with a statevector simulator rather than a unitary simulator
--- q_command.areas.lev_4_rm_2.solution_statevector =
--- {{r=0,i=0},{r=0,i=0},{r=1,i=0},{r=0,i=0}}
 
 q_command.areas.lev_4_rm_2.help_chat_msg = {
 	"Make a circuit like the last one, but negate the output as shown on the wall."
